e7fca8d6d03b9a8394d44a6eb82f415fca4e7cb1
Mena <dayjacs03@gmail.com>
2025-10-12 14:37:51 -0500

    Update

--- hosts/mena/configuration.nix
+++ hosts/mena/configuration.nix
@@ -1,6 +1,16 @@
 { config
+, username
 , ...
 }: {
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
+
+  # Define the user for nix-darwin
+  users.users.${username} = {
+    name = username;
+    home = "/Users/${username}";
+  };
+
+  # Set the state version for nix-darwin
+  system.stateVersion = 6;
 }
--- outputs/hosts.nix
+++ outputs/hosts.nix
@@ -2,14 +2,14 @@
 , ...
 }:
 let
-  inherit (import ../lib { inherit inputs; }) mkHome;
+  inherit (import ../lib { inherit inputs; }) mkSystem;
 in
 {
   imports = [ ];
 
   flake = {
-    homeConfigurations = {
-      mena = mkHome {
+    darwinConfigurations = {
+      mena = mkSystem {
         hostName = "mena";
         system = "aarch64-darwin";
         username = "mena";
