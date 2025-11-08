c2018012991f83666c3bf8cf3d7b2f73f1170857
Mena <dayjacs03@gmail.com>
2025-11-08 10:38:15 -0600

    Format on save

--- home/vscode.nix
+++ home/vscode.nix
@@ -7,11 +7,14 @@
       extensions = with pkgs.vscode-extensions; [
         catppuccin.catppuccin-vsc
         tamasfe.even-better-toml
+        esbenp.prettier-vscode
       ];
       userSettings = {
         "window.autoDetectColorScheme" = true;
         "workbench.preferredDarkColorTheme" = "Catppuccin Mocha";
         "workbench.preferredLightColorTheme" = "Catppuccin Latte";
+        "editor.formatOnSave" = true;
+        "editor.defaultFormatter" = "esbenp.prettier-vscode";
       };
     };
   };
